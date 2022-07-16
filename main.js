7cb13a804b84af995bd14d4e06944ae99acc8b1f
Reyansh2809 <77835541+Reyansh2809@users.noreply.github.com>
2022-07-16 12:38:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+leftwristX=0;
+rightwrist=0;
+difference=0;
+function setup()
+{
+canvas = createCanvas(550,500);
+canvas.position(560,150);    
+video = createCapture(VIDEO);
+video.size(550,500); 
+poseNet = ml5.poseNet(video , modelloaded); 
+poseNet.on('pose' , gotPoses);
+}
+function modelloaded()
+{
+console.log("Posenet is intialized ");    
+}
+function gotPoses(results)
+{
+if(results.length > 0)
+{
+console.log(results);
+leftwristX= results[0].pose.leftWrist.x;
+rightwristX= results[0].pose.rightWrist.x;
+difference = floor(leftwristX - rightwristX);
+}
+}
+function draw() 
+{
+    background('#FFFF00');
+    textSize(difference);
+    fill('#000009');
+    text("Reyansh" , 50 , 400);     
+}
